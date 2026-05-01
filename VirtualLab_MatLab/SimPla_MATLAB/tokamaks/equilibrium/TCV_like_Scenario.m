8c61e712a7e212b52abfae19650a74e3c1248b30
iWyss <ivanwyss94@gmail.com>
2026-05-01 12:09:44 +0200

    Uploading TCV - Like

---
+++ VirtualLab_MatLab/SimPla_MATLAB/tokamaks/equilibrium/TCV_like_Scenario.m
@@ -0,0 +1,91 @@
+function config = TCV_like_Scenario(separatrix,toroidal_current_method)
+    
+    %% Here we define the values for the target separatrix
+
+    if separatrix == 1
+        % single-null standard tokalab scenario
+        config.separatrix.scenario = 1;
+        config.separatrix.method = 1;
+        config.separatrix.k1 = 1.7;
+        config.separatrix.k2 = 2;
+        config.separatrix.d1 = 0.5;
+        config.separatrix.d2 = 0.5;
+        config.separatrix.gamma_n_1 = 0;
+        config.separatrix.gamma_n_2 = pi/3;
+        config.separatrix.gamma_p_1 = 0;
+        config.separatrix.gamma_p_2 = pi/6;
+
+        config.separatrix.a = 0.25;
+        config.separatrix.R0 = 0.85;
+        config.separatrix.Z0 = 0.1;
+
+    elseif separatrix == 2
+        % Double-null standard tokalab scenario
+        config.separatrix.scenario = 2;
+        config.separatrix.method = 1;
+        config.separatrix.k1 = 1.7;
+        config.separatrix.k2 = 1.7;
+        config.separatrix.d1 = 0.5;
+        config.separatrix.d2 = 0.5;
+        config.separatrix.gamma_n_1 = pi/3;
+        config.separatrix.gamma_n_2 = pi/3;
+        config.separatrix.gamma_p_1 = pi/6;
+        config.separatrix.gamma_p_2 = pi/6;
+         
+        config.separatrix.a = 0.7;
+        config.separatrix.R0 = 2.2;
+        config.separatrix.Z0 = 0;
+
+      elseif separatrix == 3
+        % Negative Triangularity
+        config.separatrix.scenario = 3;
+        config.separatrix.method = 1;
+        config.separatrix.k1 = 1.7;
+        config.separatrix.k2 = 2;
+        config.separatrix.d1 = -0.5;
+        config.separatrix.d2 = -0.5;
+        config.separatrix.gamma_n_1 = 0;
+        config.separatrix.gamma_n_2 = pi/6;
+        config.separatrix.gamma_p_1 = 0;
+        config.separatrix.gamma_p_2 = pi/3;
+
+        config.separatrix.a = 0.7;
+        config.separatrix.R0 = 2.2;
+        config.separatrix.Z0 = 0;
+
+    end
+
+    %% Define the toroidal current and field parameters
+
+    if toroidal_current_method == 1
+        % this method implement the function in (reference)
+        config.toroidal_current.method = 1;
+        
+        config.toroidal_current.Bt = 6;
+        config.toroidal_current.Ip = -5.5e6;
+
+        config.toroidal_current.alpha_1 = 2;
+        config.toroidal_current.alpha_2 = 2;
+        config.toroidal_current.beta_0 = 0.5;
+    
+        config.toroidal_current.lambda = 1;
+        
+    elseif toroidal_current_method == 2
+        % mwthod with reverse q profile
+        config.toroidal_current.method = 2;
+        
+        config.toroidal_current.Bt = 6;
+        config.toroidal_current.Ip = -6e6;
+
+        config.toroidal_current.alpha_1 = 2;
+        config.toroidal_current.alpha_2 = 2;
+        config.toroidal_current.beta_0 = 0.5;
+
+        config.toroidal_current.psi_n_peak = 0.3;
+    
+        config.toroidal_current.lambda = 1;
+    end
+
+
+
+end
---
+++ VirtualLab_MatLab/SimPla_MATLAB/tokamaks/geometry/TCV_like_Geometry.m
@@ -0,0 +1,18 @@
+function geo = TCV_like_Geometry()
+    
+    %% DTT Geometry
+    geo.R0 = 0.88;
+    geo.a = 0.25;
+
+    geo.grid.kappa_max = 2.8;
+    geo.grid.wall_thick = 0.2;
+
+    geo.grid.N_R = 70;
+    geo.grid.N_Z = 80;
+
+    load("DTT_like_wall.mat","Wall")
+
+    geo.wall.R = Wall(:,2)';
+    geo.wall.Z = Wall(:,1)';
+
+end
