42da3b78c59383cff9c1c7323ac51f74a67eedd0
Riccardo Rossi <riccardo.rossi.en@gmail.com>
2026-06-19 14:03:38 +0200

    notebook simpla
    Educational Notebook for SimPla

--- VirtualLab_MatLab/examples/example_01.m
+++ VirtualLab_MatLab/examples/example_01.m
@@ -18,8 +18,6 @@ geo = geo.inside_wall();
 equi = equilibrium;
 equi = equi.import_configuration(geo,tok.config);
 equi = equi.import_classes();
-% equi.config.toroidal_current.alpha_2 = 1.9;
-equi.config.separatrix.R0 = 6.6;
 equi.separatrix = equi.separatrix.build_separatrix(equi.config.separatrix,equi.geo);
 
 % show uploaded geometry and target separatrix
